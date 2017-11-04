f272255c1383a3c5ae1a0f15bb2a51c8605b9d07
lufinkey <luisfinke@gmail.com>
2017-11-04 14:34:45 -0400

    got spotify to import correctly

--- index.js
+++ index.js
@@ -1,12 +1,18 @@
 
 import { NativeModules } from 'react-native';
 
-var RCTSpotifyManager = NativeModules.RCTSpotifyManager || NativeModules.RCTSpotifyManager;
+var SpotifyManager = NativeModules.SpotifyManager || NativeModules.SpotifyManager;
 
 export default class Spotify
 {
 	constructor(client_id)
 	{
-		RCTSpotifyManager.testSpotify();
+		console.log("client_id: "+client_id);
 	}
+
+	testTheThing()
+	{
+		console.log("ayyy we doin the thing");
+		SpotifyManager.testSpotify();
+	} 
 }
