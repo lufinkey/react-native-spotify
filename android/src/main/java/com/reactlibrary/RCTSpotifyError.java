7df2229e332af90bf5a3a4cbcd8cbd548a399efb
lufinkey <luisfinke@gmail.com>
2017-11-05 17:33:35 -0500

    there was an attempt to get android to work
    but alas, fuck android

---
+++ android/src/main/java/com/reactlibrary/RCTSpotifyError.java
@@ -0,0 +1,50 @@
+package com.reactlibrary;
+
+import com.facebook.react.bridge.Arguments;
+import com.facebook.react.bridge.ReadableMap;
+import com.facebook.react.bridge.WritableMap;
+
+public class RCTSpotifyError
+{
+	public static final String DOMAIN = "RCTSpotifyErrorDomain";
+
+	public enum Code
+	{
+		ALREADY_INITIALIZED(99),
+		CONFLICTING_CALLBACKS(100);
+
+		public final int value;
+		private Code(int value)
+		{
+			this.value = value;
+		}
+	};
+
+	private Code code;
+	private String description;
+
+	public RCTSpotifyError(Code code, String description)
+	{
+		this.code = code;
+		this.description = description;
+	}
+
+	public Code getCode()
+	{
+		return code;
+	}
+
+	public String getDescription()
+	{
+		return description;
+	}
+
+	public ReadableMap toReactObject()
+	{
+		WritableMap map = Arguments.createMap();
+		map.putString("domain", DOMAIN);
+		map.putInt("code", code.value);
+		map.putString("description", description);
+		return map;
+	}
+}
--- android/src/main/java/com/reactlibrary/RCTSpotifyModule.java
+++ android/src/main/java/com/reactlibrary/RCTSpotifyModule.java
@@ -1,22 +1,83 @@
 
 package com.reactlibrary;
 
+import com.facebook.react.bridge.LifecycleEventListener;
+import com.facebook.react.bridge.Promise;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
-import com.facebook.react.bridge.Callback;
+import com.facebook.react.bridge.ReadableMap;
+import com.facebook.react.bridge.WritableMap;
+import com.facebook.react.bridge.WritableNativeMap;
 
-public class RCTSpotifyModule extends ReactContextBaseJavaModule {
+import android.app.Activity;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 
-  private final ReactApplicationContext reactContext;
+import com.spotify.sdk.android.authentication.AuthenticationClient;
+import com.spotify.sdk.android.authentication.AuthenticationRequest;
+import com.spotify.sdk.android.authentication.AuthenticationResponse;
+import com.spotify.sdk.android.player.Config;
+import com.spotify.sdk.android.player.ConnectionStateCallback;
+import com.spotify.sdk.android.player.Connectivity;
+import com.spotify.sdk.android.player.Error;
+import com.spotify.sdk.android.player.Metadata;
+import com.spotify.sdk.android.player.PlaybackBitrate;
+import com.spotify.sdk.android.player.PlaybackState;
+import com.spotify.sdk.android.player.Player;
+import com.spotify.sdk.android.player.PlayerEvent;
+import com.spotify.sdk.android.player.Spotify;
+import com.spotify.sdk.android.player.SpotifyPlayer;
 
-  public RCTSpotifyModule(ReactApplicationContext reactContext) {
-    super(reactContext);
-    this.reactContext = reactContext;
-  }
+public class RCTSpotifyModule extends ReactContextBaseJavaModule
+{
+	private final ReactApplicationContext reactContext;
 
-  @Override
-  public String getName() {
-    return "RCTSpotify";
-  }
+	private boolean initialized;
+
+	private SpotifyPlayer player;
+	private BroadcastReceiver networkStateReceiver;
+
+	public RCTSpotifyModule(ReactApplicationContext reactContext)
+	{
+		super(reactContext);
+		this.reactContext = reactContext;
+
+		initialized = false;
+		player = null;
+		networkStateReceiver = null;
+	}
+
+	@Override
+	public String getName()
+	{
+		return "RCTSpotify";
+	}
+
+	@ReactMethod
+	void test()
+	{
+		System.out.println("ayy lmao");
+	}
+
+	@ReactMethod
+	//initialize(options, (
+	void initialize(ReadableMap options, Callback completion)
+	{
+		if(initialized)
+		{
+			completion.invoke(
+					false,
+					new RCTSpotifyError(
+							RCTSpotifyError.Code.ALREADY_INITIALIZED,
+							"cannot initialize Spotify times").toReactObject()
+			);
+			return;
+		}
+		//TODO do the thing
+	}
 }
