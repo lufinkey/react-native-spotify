87b136a9362bb6e9d3cd67b0df36cf6b30e0393a
lufinkey <luisfinke@gmail.com>
2017-11-30 13:04:08 -0500

    remove unnecessary thing

--- android/src/main/java/com/lufinkey/react/spotify/RCTSpotifyModule.java
+++ android/src/main/java/com/lufinkey/react/spotify/RCTSpotifyModule.java
@@ -134,10 +134,6 @@ public class RCTSpotifyModule extends ReactContextBaseJavaModule implements Play
 		{
 			scopes = options.getArray("scopes");
 		}
-		else
-		{
-			scopes = Arguments.createArray();
-		}
 		if(scopes!=null)
 		{
 			String[] requestedScopes = new String[scopes.size()];
