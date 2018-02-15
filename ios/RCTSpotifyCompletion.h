5f59060129da9f8156732a81fbf7985b89d1092a
lufinkey <luisfinke@gmail.com>
2018-02-15 09:29:20 -0500

    RCTSpotifyCompletion empty class

---
+++ ios/RCTSpotifyCompletion.h
@@ -0,0 +1,13 @@
+//
+//  RCTSpotifyCompletion.h
+//  RCTSpotify
+//
+//  Created by Luis Finke on 2/15/18.
+//  Copyright © 2018 Facebook. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface RCTSpotifyCompletion : NSObject
+
+@end
---
+++ ios/RCTSpotifyCompletion.m
@@ -0,0 +1,13 @@
+//
+//  RCTSpotifyCompletion.m
+//  RCTSpotify
+//
+//  Created by Luis Finke on 2/15/18.
+//  Copyright © 2018 Facebook. All rights reserved.
+//
+
+#import "RCTSpotifyCompletion.h"
+
+@implementation RCTSpotifyCompletion
+
+@end
