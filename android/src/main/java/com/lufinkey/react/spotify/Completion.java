60f2e9b7fe80ae2dc5e6317b3417dcd16c015872
lufinkey <luisfinke@gmail.com>
2018-02-16 08:26:20 -0500

    prevent subsequent calls to resolve or reject

--- android/src/main/java/com/lufinkey/react/spotify/Completion.java
+++ android/src/main/java/com/lufinkey/react/spotify/Completion.java
@@ -2,14 +2,26 @@ package com.lufinkey.react.spotify;
 
 public abstract class Completion<T>
 {
+	private boolean responded = false;
+
 	public final void resolve(T result)
 	{
+		if(responded)
+		{
+			throw new IllegalStateException("cannot call resolve or reject multiple times on a Completion object");
+		}
+		responded = true;
 		onResolve(result);
 		onComplete(result, null);
 	}
 
 	public final void reject(SpotifyError error)
 	{
+		if(responded)
+		{
+			throw new IllegalStateException("cannot call resolve or reject multiple times on a Completion object");
+		}
+		responded = true;
 		onReject(error);
 		onComplete(null, error);
 	}
