71116583c47db652ee6ef5c0a1fcf1939dc6ddb3
lufinkey <luisfinke@gmail.com>
2018-03-24 21:47:54 -0400

    link react-native-events in example app

--- example/android/app/src/main/java/com/examplespotifyapp/MainApplication.java
+++ example/android/app/src/main/java/com/examplespotifyapp/MainApplication.java
@@ -3,6 +3,7 @@ package com.examplespotifyapp;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.lufinkey.react.eventemitter.RNEventEmitterPackage;
 import com.lufinkey.react.spotify.RNSpotifyPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
@@ -24,6 +25,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new RNEventEmitterPackage(),
             new RNSpotifyPackage()
       );
     }
