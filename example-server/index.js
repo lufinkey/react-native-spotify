29f9e33a6cc45034888186324f8bdcb6f82f3b68
lufinkey <luisfinke@gmail.com>
2018-03-24 22:49:01 -0400

    reduce example-server dependencies

--- example-server/index.js
+++ example-server/index.js
@@ -1,9 +1,9 @@
 // npm deps
-const fetch = require('node-fetch');
 const express = require('express');
-const bodyParser = require('body-parser');
-const {URLSearchParams} = require('url');
+const https = require('https');
 const crypto = require('crypto');
+const { URL } = require('url');
+const QueryString = require('querystring');
 
 // Require the framework and instantiate it
 const app = express();
@@ -12,56 +12,129 @@ const app = express();
 const spClientId = process.env.SPOTIFY_CLIENT_ID;
 const spClientSecret = process.env.SPOTIFY_CLIENT_SECRET;
 const spClientCallback = process.env.SPOTIFY_CLIENT_CALLBACK;
-const authString = Buffer.from(`${spClientId}:${spClientSecret}`).toString('base64');
-const authorizationHeader = `Basic ${authString}`;
+const spServerPort = process.env.PORT ? parseInt(process.env.PORT) : 3000;
+const authString = Buffer.from(spClientId+':'+spClientSecret).toString('base64');
+const authHeader = `Basic ${authString}`;
 const spotifyEndpoint = 'https://accounts.spotify.com/api/token';
 
 // encryption
 const encSecret = process.env.ENCRYPTION_SECRET;
-const encrypt = text => {
-	const aes = crypto.createCipher('aes-256-ctr', encSecret);
+const encMethod = process.env.ENCRYPTION_METHOD || "aes-256-ctr";
+const encrypt = (text) => {
+	const aes = crypto.createCipher(encMethod, encSecret);
 	let encrypted = aes.update(text, 'utf8', 'hex');
 	encrypted += aes.final('hex');
 	return encrypted;
 };
-const decrypt = text => {
-	const aes = crypto.createDecipher('aes-256-ctr', encSecret);
+const decrypt = (text) => {
+	const aes = crypto.createDecipher(encMethod, encSecret);
 	let decrypted = aes.update(text, 'hex', 'utf8');
 	decrypted += aes.final('utf8');
 	return decrypted;
 };
 
+// handle sending POST request
+function postRequest(url, data={})
+{
+	return new Promise((resolve, reject) => {
+		// build request data
+		url = new URL(url);
+		const reqData = {
+			protocol: url.protocol,
+			hostname: url.hostname,
+			port: url.port,
+			path: url.pathname,
+			method: 'POST',
+			headers: {
+				'Authorization': authHeader,
+				'Content-Type': 'application/x-www-form-urlencoded'
+			}
+		}
+		console.log("sending reqData:");
+		console.log(reqData);
+		console.log("");
+
+		// create request
+		const req = https.request(reqData, (res) => {
+			// build response
+			let buffers = [];
+			res.on('data', (chunk) => {
+				buffers.push(chunk);
+			});
+
+			res.on('end', () => {
+				// parse response
+				var result = null;
+				try
+				{
+					result = Buffer.concat(buffers);
+					result = result.toString();
+					var contentType = res.headers['content-type'];
+					if(typeof contentType == 'string')
+					{
+						contentType = contentType.split(';')[0].trim();
+					}
+					if(contentType == 'application/x-www-form-urlencoded')
+					{
+						result = QueryString.parse(result);
+					}
+					else if(contentType == 'application/json')
+					{
+						result = JSON.parse(result);
+					}
+				}
+				catch(error)
+				{
+					console.error(error);
+					reject(error);
+					return;
+				}
+				console.log("got result:");
+				console.log(result);
+				resolve(result);
+			})
+		});
+
+		// handle error
+		req.on('error', (error) => {
+			reject(error);
+		});
+
+		// send
+		data = QueryString.stringify(data);
+		console.log("sending data:");
+		console.log(data);
+		console.log("");
+		req.write(data);
+		req.end();
+	});
+}
+
 // support form body
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.urlencoded());
 
 /**
  * Swap endpoint
  * Uses an authentication code on body to request access and refresh tokens
  */
 app.post('/swap', async (req, res) => {
-	const data = new URLSearchParams();
-	data.append('grant_type', 'authorization_code');
-	data.append('redirect_uri', spClientCallback);
-	data.append('code', req.body.code);
+	console.log("/swap called");
+	const data = {
+		grant_type: 'authorization_code',
+		redirect_uri: spClientCallback,
+		code: req.body.code
+	};
 
 	// get tokens from spotify api
-	const result = await fetch(spotifyEndpoint, {
-		method: 'POST',
-		headers: {
-			Authorization: authorizationHeader,
-			'Content-Type': 'application/x-www-form-urlencoded',
-		},
-		body: data,
-	});
-	const replyBody = await result.json();
+	const result = await postRequest(spotifyEndpoint, data);
 
 	// encrypt refresh_token
-	if (replyBody.refresh_token) {
-		replyBody.refresh_token = encrypt(replyBody.refresh_token);
+	if (result.refresh_token) {
+		result.refresh_token = encrypt(result.refresh_token);
 	}
 
 	// send response
-	res.send(replyBody);
+	res.send(result);
 });
 
 /**
@@ -69,6 +142,7 @@ app.post('/swap', async (req, res) => {
  * Uses the refresh token on request body to get a new access token
  */
 app.post('/refresh', async (req, res) => {
+	console.log("/refresh called");
 	if (!req.body.refresh_token) {
 		res.status(400).send({error: 'Refresh token is missing from body'});
 		return;
@@ -77,27 +151,20 @@ app.post('/refresh', async (req, res) => {
 	// decrypt token
 	const refreshToken = decrypt(req.body.refresh_token);
 	// prepare data for request
-	const data = new URLSearchParams();
-	data.append('grant_type', 'refresh_token');
-	data.append('refresh_token', refreshToken);
+	const data = {
+		grant_type: 'refresh_token',
+		refresh_token: refreshToken
+	};
 	// get new token from Spotify API
-	const result = await fetch(spotifyEndpoint, {
-		method: 'POST',
-		headers: {
-			Authorization: authorizationHeader,
-			'Content-Type': 'application/x-www-form-urlencoded',
-		},
-		body: data,
-	});
-	const replyBody = await result.json();
+	const result = postRequest(spotifyEndpoint, data);
 
 	// encrypt refresh_token
-	if (replyBody.refresh_token) {
-		replyBody.refresh_token = encrypt(replyBody.refresh_token);
+	if (result.refresh_token) {
+		result.refresh_token = encrypt(result.refresh_token);
 	}
 
 	// send response
-	res.status(res.status).send(replyBody);
+	res.status(res.status).send(result);
 });
 
-app.listen(3000, () => console.log('Example app listening on port 3000!'));
+app.listen(spServerPort, () => console.log('Example app listening on port '+spServerPort+'!'));
