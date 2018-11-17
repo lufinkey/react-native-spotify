0724339d38181be8fead61e506517ff69d389d65
lufinkey <luisfinke@gmail.com>
2018-11-16 20:58:10 -0500

    update example project

--- example/android/app/src/main/java/com/examplespotifyapp/MainApplication.java
+++ example/android/app/src/main/java/com/examplespotifyapp/MainApplication.java
@@ -3,8 +3,8 @@ package com.examplespotifyapp;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
-import com.lufinkey.react.eventemitter.RNEventEmitterPackage;
 import com.lufinkey.react.spotify.RNSpotifyPackage;
+import com.lufinkey.react.eventemitter.RNEventEmitterPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
@@ -25,8 +25,8 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
-            new RNEventEmitterPackage(),
-            new RNSpotifyPackage()
+            new RNSpotifyPackage(),
+            new RNEventEmitterPackage()
       );
     }
 
