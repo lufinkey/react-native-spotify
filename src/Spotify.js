435ad2ca1ee71defdb3c704381517a6f0a4099dc
Luis Finke <lfinke@fusionalliance.com>
2019-01-14 16:38:49 -0500

    add empty options by default on methods

--- src/Spotify.js
+++ src/Spotify.js
@@ -18,178 +18,179 @@ Spotify.getMe = () => {
 
 
 Spotify.getMyPlaylists = (options) => {
-	var body = Object.assign({}, options);
+	const body = {...options};
 	return sendRequest('v1/me/playlists', 'GET', body, false);
 }
 
 
 
 Spotify.getMyTracks = (options) => {
-	var body = Object.assign({}, options);
+	const body = {...options};
 	return sendRequest('v1/me/tracks', 'GET', body, false);
 }
 
 
 
 Spotify.search = (query, types, options) => {
-	if(!(types instanceof Array))
-	{
+	if(!(types instanceof Array)) {
 		return Promise.reject(new Error("types must be an array"));
 	}
-
-	var body = Object.assign({}, options);
+	const body = {...options};
 	body['q'] = query;
 	body['type'] = types.join(',');
-	
-	return sendRequest('v1/search', 'GET', body, false);
+	const results = sendRequest('v1/search', 'GET', body, false);
+	for(const type in ['tracks','albums','artists','playlists']) {
+		const typeResults = results[type];
+		if(typeResults) {
+			// heck you, Spotify
+			if(typeResults.next === 'null') {
+				typeResults.next = null;
+			}
+			if(typeResults.prev === 'null') {
+				typeResults.prev = null;
+			}
+		}
+	}
+	return results;
 }
 
 
 
 Spotify.getPlaylistTracks = (userId, playlistId, options) => {
-	var body = Object.assign({}, options);
+	const body = {...options};
 	return sendRequest(`v1/users/${userId}/playlists/${playlistId}/tracks`, 'GET', body, false);
 }
 
 
 
 Spotify.getAlbum = (albumID, options) => {
-	if(albumID == null)
-	{
+	if(albumID == null) {
 		return Promise.reject(new Error("albumID cannot be null"));
 	}
-	
 	return sendRequest('v1/albums/'+albumID, 'GET', options, false);
 }
 
 Spotify.getAlbums = (albumIDs, options) => {
-	if(!(albumIDs instanceof Array))
-	{
+	if(!(albumIDs instanceof Array)) {
 		return Promise.reject(new Error("albumIDs must be an array"));
 	}
-
-	var body = Object.assign({}, options);
+	const body = {...options};
 	body['ids'] = albumIDs.join(',');
-
 	return sendRequest('v1/albums', 'GET', body, false);
 }
 
 Spotify.getAlbumTracks = (albumID, options) => {
-	if(albumID == null)
-	{
+	if(albumID == null) {
 		return Promise.reject(new Error("albumID cannot be null"));
 	}
-
-	return sendRequest('v1/albums/'+albumID+'/tracks', 'GET', options, false);
+	const body = {...options};
+	const results = sendRequest('v1/albums/'+albumID+'/tracks', 'GET', body, false);
+	// fix Spotify's mistakes
+	if(results.next === 'null') {
+		results.next = null;
+	}
+	if(results.prev === 'null') {
+		results.prev = null;
+	}
+	return results;
 }
 
 
 
 Spotify.getArtist = (artistID, options) => {
-	if(artistID == null)
-	{
+	if(artistID == null) {
 		return Promise.reject(new Error("artistID cannot be null"));
 	}
-
-	return sendRequest('v1/artists/'+artistID, 'GET', options, false);
+	const body = {...options};
+	return sendRequest('v1/artists/'+artistID, 'GET', body, false);
 }
 
 Spotify.getArtists = (artistIDs, options) => {
-	if(!(artistIDs instanceof Array))
-	{
+	if(!(artistIDs instanceof Array)) {
 		return Promise.reject(new Error("artistIDs must be an array"));
 	}
-
-	var body = Object.assign({}, options);
+	const body = {...options};
 	body['ids'] = artistIDs.join(',');
-
 	return sendRequest('v1/artists', 'GET', body, false);
 }
 
 Spotify.getArtistAlbums = (artistID, options) => {
-	if(artistID == null)
-	{
+	if(artistID == null) {
 		return Promise.reject(new Error("artistID cannot be null"));
 	}
-
-	return sendRequest('v1/artists/'+artistID+'/albums', 'GET', options, false);
+	const body = {...options};
+	const results = sendRequest('v1/artists/'+artistID+'/albums', 'GET', body, false);
+	// fix Spotify's bullshit
+	if(results.next === 'null') {
+		results.next = null;
+	}
+	if(results.prev === 'null') {
+		results.prev = null;
+	}
+	return results;
 }
 
 Spotify.getArtistTopTracks = (artistID, country, options) => {
-	if(artistID == null)
-	{
+	if(artistID == null) {
 		return Promise.reject(new Error("artistID cannot be null"));
 	}
-	else if(country == null)
-	{
+	else if(country == null) {
 		return Promise.reject(new Error("country cannot be null"));
 	}
-
-	var body = Object.assign({}, options);
+	const body = {...options};
 	body['country'] = country;
-
 	return sendRequest('v1/artists/'+artistID+'/top-tracks', 'GET', body, false);
 }
 
 Spotify.getArtistRelatedArtists = (artistID, options) => {
-	if(artistID == null)
-	{
+	if(artistID == null) {
 		return Promise.reject(new Error("artistID cannot be null"));
 	}
-	
-	return sendRequest('v1/artists/'+artistID+'/related-artists', 'GET', options, false);
+	const body = {...options};
+	return sendRequest('v1/artists/'+artistID+'/related-artists', 'GET', body, false);
 }
 
 
 
 Spotify.getTrack = (trackID, options) => {
-	if(trackID == null)
-	{
+	if(trackID == null) {
 		return Promise.reject(new Error("trackID cannot be null"));
 	}
-
-	return sendRequest('v1/tracks/'+trackID, 'GET', options, false);
+	const body = {...options};
+	return sendRequest('v1/tracks/'+trackID, 'GET', body, false);
 }
 
 Spotify.getTracks = (trackIDs, options) => {
-	if(!(trackIDs instanceof Array))
-	{
+	if(!(trackIDs instanceof Array)) {
 		return Promise.reject(new Error("trackIDs must be an array"));
 	}
-
-	var body = Object.assign({}, options);
+	const body = {...options};
 	body['ids'] = trackIDs.join(',');
-
 	return sendRequest('v1/tracks', 'GET', body, false);
 }
 
 Spotify.getTrackAudioAnalysis = (trackID, options) => {
-	if(trackID == null)
-	{
+	if(trackID == null) {
 		return Promise.reject(new Error("trackID cannot be null"));
 	}
-
-	return sendRequest('v1/audio-analysis/'+trackID, 'GET', options, false);
+	const body = {...options};
+	return sendRequest('v1/audio-analysis/'+trackID, 'GET', body, false);
 }
 
 Spotify.getTrackAudioFeatures = (trackID, options) => {
-	if(trackID == null)
-	{
+	if(trackID == null) {
 		return Promise.reject(new Error("trackID cannot be null"));
 	}
-
-	return sendRequest('v1/audio-features/'+trackID, 'GET', options, false);
+	const body = {...options};
+	return sendRequest('v1/audio-features/'+trackID, 'GET', body, false);
 }
 
 Spotify.getTracksAudioFeatures = (trackIDs, options) => {
-	if(!(trackIDs instanceof Array))
-	{
+	if(!(trackIDs instanceof Array)) {
 		return Promise.reject(new Error("trackIDs must be an array"));
 	}
-
-	var body = Object.assign({}, options);
+	const body = {...options};
 	body['ids'] = trackIDs.join(',');
-
 	return sendRequest('v1/audio-features', 'GET', body, false);
 }
 
