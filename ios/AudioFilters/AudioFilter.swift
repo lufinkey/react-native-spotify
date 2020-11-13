9be912d87762ca6eaf85fb8e73bd35c6047c19d6
Freddy Snijder <freddy@visionscapers.com>
2020-11-13 01:22:03 +0100

    Added public specifier to make interfaces visible to host project when used as pod (framework)

--- ios/AudioFilters/AudioFilter.swift
+++ ios/AudioFilters/AudioFilter.swift
@@ -13,10 +13,10 @@ func sizeof<T:FixedWidthInteger>(_ int:T) -> Int {
     return int.bitWidth/UInt8.bitWidth
 }
 
-@objc protocol AudioFilter {
-  var _equalizer: AudioUnit { get set }
-  var _isEnabled: Bool { get set }
-  
+@objc public protocol AudioFilter {
+  var audioEqualizer: AudioUnit {get}
+  var isEnabled: Bool {get}
+
   func setup() -> Bool
   
   func reset() -> Bool
@@ -27,9 +27,8 @@ func sizeof<T:FixedWidthInteger>(_ int:T) -> Int {
 }
 
 extension AudioFilter {
-  
   func _setParam(parameter: AudioUnitParameterID, toValue: AudioUnitParameterValue, description: String) -> Bool {
-    let state = AudioUnitSetParameter(_equalizer, parameter, kAudioUnitScope_Global, 0, toValue, 0);
+    let state = AudioUnitSetParameter(audioEqualizer, parameter, kAudioUnitScope_Global, 0, toValue, 0);
     if (state != noErr) {
       _log(methodName: "\(#function)", message: "unable to \(description): OSState = \(state)");
       return false;
@@ -39,7 +38,7 @@ extension AudioFilter {
   }
 
   func _setProp(parameter: AudioUnitPropertyID, toValue: UnsafeRawPointer, valueSize: UInt32, description: String) -> Bool {
-    let state = AudioUnitSetProperty(_equalizer, parameter, kAudioUnitScope_Global, 0, toValue, valueSize);
+    let state = AudioUnitSetProperty(audioEqualizer, parameter, kAudioUnitScope_Global, 0, toValue, valueSize);
     if (state != noErr) {
       _log(methodName: "\(#function)", message: "unable to \(description): OSState = \(state)");
       return false;
