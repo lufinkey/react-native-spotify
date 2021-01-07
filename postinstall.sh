56847158e20eb0e45db2362655e26546cb21cf44
Jacobus Meulen <sjaakiejj@gmail.com>
2021-01-07 02:40:25 +0100

    Fix yarn install (#3)
    * Add a postinstall script
    
    * Clone to the right folders
    
    * Fixed broken error condition on git pull
    
    Co-authored-by: Sjaak Meulen <jacobus@kadima-tech.com>

---
+++ postinstall.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+MODULES=`sed -n -e '/path/,/url/p' .gitmodules | sed 'N;s/\\n/\\$$$/'`
+echo "$MODULES" | while IFS= read -r line; do
+  if [[ $line =~ (.*)\$\$\$(.*) ]] ; then
+    path=$(echo ${BASH_REMATCH[1]} | sed 's/.*= //')
+    url=$(echo ${BASH_REMATCH[2]} | sed 's/.*= //')
+    echo $PWD
+    echo $CWD
+    if [ -f "$path/.git" ] ; then
+      echo "Updating submodule"
+      start=$PWD;
+      (cd $path && git pull && cd $PWD)
+    else
+    echo $path
+      echo "Cloning submodule $url to $path" "$path/.git"
+      git clone $url $path
+    fi
+  fi
+done
